2a65776c8c689e7ae3f9c73233f0c0f224326ad3
Andreyev Kyrylo <K.Andreyev.FIT.121.20@knute.edu.ua>
2021-09-22 15:45:29 +0300

    Add Calculator class and implement +, -, * operations

---
+++ OOP_Lab3/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Lab3
+{
+    public class Calculator
+    {
+        // Сложение
+        private double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        // Вычитание
+        private double Substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        // Умножение
+        private double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
