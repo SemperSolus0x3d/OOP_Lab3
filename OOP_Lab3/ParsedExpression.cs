eb35df4ea30ff8a7e22f1d3ebef96cfdbc3a15d2
Andreyev Kyrylo <K.Andreyev.FIT.121.20@knute.edu.ua>
2021-09-23 22:10:18 +0300

    Implement ParsedExpression class

---
+++ OOP_Lab3/ParsedExpression.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Lab3
+{
+    public enum Operation
+    {
+        Addition,
+        Substraction,
+        Multiplication
+    }
+
+    public class ParsedExpression
+    {
+        public decimal LeftOperand { get; }
+        public Operation Operation { get; }
+        public decimal RightOperand { get; }
+
+        public ParsedExpression(
+            decimal leftOperand, 
+            Operation operation, 
+            decimal rightOperand
+        )
+        {
+            LeftOperand = leftOperand;
+            Operation = operation;
+            RightOperand = rightOperand;
+        }
+    }
+}
